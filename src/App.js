4197e2b6d41dd28bc20dd288a8e50e6fb806a546
moshuai <852456378@qq.com>
2019-03-05 09:54:08 +0800

    public/index.html

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import PropTypes from 'prop-types'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className="container">
+        <h1 className="jumbotron-heading text-center">{this.props.value}</h1>
+        <p className="text-center">
+        <button onClick={this.props.onIncream}  className="btn btn-primary mr-2">Increase</button>
+        <button onClick={this.props.deCrease} className="btn btn-danger my-2">Decrease</button>
+        </p>
+        </div>
     );
   }
+}
+
+App.propTypes={
+            value:PropTypes.number.isRequired,
+       onIncream:PropTypes.func.isRequired,
+        deCrease:PropTypes.func.isRequired
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,51 @@
 import React from 'react';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import reducer from './reducers/counter';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store=createStore(reducer);
+store.subscribe(()=>console.log("state updated", store.getState()));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+//function user(state = {name: 'redux'}, action) {
+//    switch (action.type) {
+//        case 'CHANGE_NAME':
+//            return {
+//                ...state,
+//                name: action.name
+//            }
+//    }
+//
+//    return state
+//}
+//function project(state = {name: 'min-react'}, action) {
+//    switch (action.type) {
+//        case 'CHANGE_NAME':
+//            return {
+//                ...state,
+//                name: action.name
+//            }
+//    }
+//
+//    return state
+//}
+//var rootReducer = combineReducers({
+//    user,
+//    project
+//})
+//
+//var store = createStore(rootReducer)
+//
+//console.log(store.getState())
+
+
+const render =()=>{
+    ReactDOM.render(<App onIncream={()=>store.dispatch({type:"INCREMENT"})} deCrease={()=>store.dispatch({type:"DECREMENT"})} value={store.getState()} />, document.getElementById('root'));
+}
+
+render();
+
+store.subscribe(render);
 serviceWorker.unregister();
